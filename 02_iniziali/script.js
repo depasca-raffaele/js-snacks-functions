334fecefff03f63d4fa77e6b888abab28abe6119
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-01 14:54:47 +0200

    esercizio 2, funzione classica ed arrow

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function getIniziali(array){
+    let iniziali = [];
+    for(let i = 0; i < array.length; i++){
+        iniziali.push(array[i].charAt(0));
+    }
+    return iniziali;
+}
+
+const getInizialiArrow = (array) => {
+     let iniziali = [];
+    for(let i = 0; i < array.length; i++){
+        iniziali.push(array[i].charAt(0));
+    }
+    return iniziali;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let risultato = getInizialiArrow(names);
+console.log(risultato);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
